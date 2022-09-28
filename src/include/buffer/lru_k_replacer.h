d1b704566185ce5fbdccef7d424ffaf1a0999d45
xincheng <80968861+xinchengxx@users.noreply.github.com>
2022-09-29 02:40:01 +0800

    fix(typo): fix the typo (#400)
    Signed-off-by: xinchengxx <2240131203@qq.com>

--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -143,7 +143,7 @@ struct FrameComp {
  * current timestamp and the timestamp of kth previous access.
  *
  * A frame with less than k historical references is given
- * +inf as its backward k-distance. When multipe frames have +inf backward k-distance,
+ * +inf as its backward k-distance. When multiple frames have +inf backward k-distance,
  * classical LRU algorithm is used to choose victim.
  */
 class LRUKReplacer {
