9490ecce8ff3f65be5de8063a9b432978e310fc9
YukunJ <yukunj@andrew.cmu.edu>
2022-11-02 16:38:04 -0400

    Imple: index join

--- src/execution/nested_index_join_executor.cpp
+++ src/execution/nested_index_join_executor.cpp
@@ -16,15 +16,55 @@ namespace bustub {
 
 NestIndexJoinExecutor::NestIndexJoinExecutor(ExecutorContext *exec_ctx, const NestedIndexJoinPlanNode *plan,
                                              std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {
-  if (!(plan->GetJoinType() == JoinType::LEFT || plan->GetJoinType() == JoinType::INNER)) {
+    : AbstractExecutor(exec_ctx),
+      plan_(plan),
+      child_executor_(std::move(child_executor)),
+      inner_schema_(plan_->InnerTableSchema()),
+      outer_schema_(plan_->GetChildPlan()->OutputSchema()),
+      key_schema_(std::vector<Column>{{"index_key", plan->KeyPredicate()->GetReturnType()}}) {
+  if (plan->GetJoinType() != JoinType::LEFT && plan->GetJoinType() != JoinType::INNER) {
     // Note for 2022 Fall: You ONLY need to implement left join and inner join.
     throw bustub::NotImplementedException(fmt::format("join type {} not supported", plan->GetJoinType()));
   }
 }
 
-void NestIndexJoinExecutor::Init() { throw NotImplementedException("NestIndexJoinExecutor is not implemented"); }
+void NestIndexJoinExecutor::Init() {
+  child_executor_->Init();
+  auto index_oid = plan_->GetIndexOid();
+  index_ = exec_ctx_->GetCatalog()->GetIndex(index_oid);
+  match_rids_.clear();
+  inner_table_ptr_ = exec_ctx_->GetCatalog()->GetTable(plan_->GetInnerTableOid());
+}
 
-auto NestIndexJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto NestIndexJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  while (true) {
+    // Outer table cursor always increment by 1 and try to find a match
+    auto status = child_executor_->Next(&outer_tuple_, &outer_rid_);
+    if (!status) {
+      // join finish
+      return false;
+    }
+    std::vector<Value> key_value{plan_->KeyPredicate()->Evaluate(&outer_tuple_, outer_schema_)};
+    auto key_index = Tuple(key_value, &key_schema_);
+    index_->index_->ScanKey(key_index, &match_rids_, exec_ctx_->GetTransaction());
+    if (!match_rids_.empty()) {
+      // by assumption, can have only 1 match in indexed table
+      inner_rid_ = match_rids_.back();
+      match_rids_.clear();
+      inner_table_ptr_->table_->GetTuple(inner_rid_, &inner_tuple_, exec_ctx_->GetTransaction());
+      std::vector<Value> value;
+      MergeValueFromTuple(value, false);
+      *tuple = Tuple(value, &GetOutputSchema());
+      return true;
+    }
+    if (plan_->GetJoinType() == JoinType::LEFT) {
+      std::vector<Value> value;
+      MergeValueFromTuple(value, true);  // null value placeholder for inner table
+      *tuple = Tuple(value, &GetOutputSchema());
+      return true;
+    }
+  }
+  return false;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/nested_index_join_executor.h
+++ src/include/execution/executors/nested_index_join_executor.h
@@ -24,6 +24,7 @@
 #include "execution/plans/nested_index_join_plan.h"
 #include "storage/table/tmp_tuple.h"
 #include "storage/table/tuple.h"
+#include "type/value_factory.h"
 
 namespace bustub {
 
@@ -48,7 +49,45 @@ class NestIndexJoinExecutor : public AbstractExecutor {
   auto Next(Tuple *tuple, RID *rid) -> bool override;
 
  private:
+  /** Merge the current left_tuple and right_tuple's value into one vector for new tuple creation */
+  void MergeValueFromTuple(std::vector<Value> &value, bool right_null) const {
+    auto left_column_count = outer_schema_.GetColumnCount();
+    auto right_column_count = inner_schema_.GetColumnCount();
+    for (unsigned int i = 0; i < left_column_count; i++) {
+      value.push_back(outer_tuple_.GetValue(&outer_schema_, i));
+    }
+    for (unsigned int i = 0; i < right_column_count; i++) {
+      if (!right_null) {
+        value.push_back(inner_tuple_.GetValue(&inner_schema_, i));
+      } else {
+        value.push_back(ValueFactory::GetNullValueByType(inner_schema_.GetColumn(i).GetType()));
+      }
+    }
+  }
+
   /** The nested index join plan node. */
   const NestedIndexJoinPlanNode *plan_;
+  /** The child executor from which to fetch join tuple */
+  std::unique_ptr<AbstractExecutor> child_executor_;
+  /** The index of the Inner table */
+  IndexInfo *index_{nullptr};
+  /** the current cursor in the outer table */
+  Tuple outer_tuple_{};
+  /** the current RID in the outer table tuple */
+  RID outer_rid_{};
+  /** the current cursor in the inner table */
+  Tuple inner_tuple_{};
+  /** the current RID in the inner table tuple */
+  RID inner_rid_{};
+  /** the index matching rids in the inner table */
+  std::vector<RID> match_rids_{};
+  /** pointer to the inner table */
+  TableInfo *inner_table_ptr_{nullptr};
+  /** Inner Table Schema */
+  Schema inner_schema_;
+  /** Outer Table Schema */
+  Schema outer_schema_;
+  /** Key Schema */
+  Schema key_schema_;
 };
 }  // namespace bustub
